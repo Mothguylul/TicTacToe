d96f9b1c6e20c514d627da86e8bf0aef892098be
mothguylul <juliusmuench89gmail.com>
2024-11-07 18:06:18 +0100

    Working Project

---
+++ TicTacToe.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using TicTacToe.Logic;
+
+class Program
+{
+	static void Main()
+	{
+		GameLogic main = new GameLogic();
+
+		for (int i = 0; i < 9; i++)
+		{
+			int userinput;
+			Console.WriteLine("Enter a Number between 1 - 9. " +
+				"\n|7|8|9|\n|4|5|6|\n|1|2|3|");
+
+			while (true)
+			{
+				if (int.TryParse(Console.ReadLine(), out userinput) && main.TakeInput(userinput))
+				{
+					break;//valid input
+				}
+
+				Console.WriteLine("Invalid input or field already chosen. Please try again.");
+			}
+
+			//check for win 
+			var winCheck = main.CheckForWin();
+			if (winCheck.IsWin)
+			{
+				Console.WriteLine($"{winCheck.WinningPlayer} won!");
+				main.Restart();
+				break;
+			}
+			else
+			{
+				if (i == 8)
+				{
+					Console.WriteLine("Its a Draw.");
+					main.Restart();
+					break;
+				}
+
+				Console.WriteLine("No winner yet. Next player's turn.");
+				main.SwitchPlayers();
+				Console.WriteLine(main.Turn == Players.Player1 ? "Player 1 turn" : "Player 2 turn");
+			}
+		}
+	}
+}
---
+++ TicTacToe.Logic/GameLogic.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Logic
+{
+	public class GameLogic
+	{
+		public List<int> Player1Inputs = new List<int>();
+
+		public List<int> Player2Inputs = new List<int>();
+
+		public readonly List<int[]> winningCombinations = new List<int[]>
+		{
+			new int[] { 1, 2, 3 },
+			new int[] { 4, 5, 6 },
+			new int[] { 7, 8, 9 },
+			new int[] { 1, 4, 7 },
+			new int[] { 2, 5, 8 },
+			new int[] { 3, 6, 9 },
+			new int[] { 1, 5, 9 },
+			new int[] { 3, 5, 7 }
+		};
+
+		public Players Turn { get; set; }
+
+		public GameLogic()
+		{
+			Turn = Players.Player1;
+		}
+
+		public bool TakeInput(int input)
+		{
+			int userinput = input;
+
+			if (userinput > 9 || userinput < 1 || FieldIsAlreadyChoosen(userinput))
+			{
+				return false;
+			}
+			else
+			{
+				if (Turn == Players.Player1)
+				{
+					Player1Inputs.Add(userinput);
+				}
+
+				else if (Turn == Players.Player2)
+				{
+					Player2Inputs.Add(userinput);
+				}
+				return true;
+			}
+		}
+
+		public bool FieldIsAlreadyChoosen(int currentInput)
+		{
+			/*
+
+			if (Turn == Players.Player1)
+			{
+				int? searchCurrentInput = Player1Inputs.FirstOrDefault(i => currentInput == currentInput);
+
+				return searchCurrentInput == null ? false : true;
+
+			}
+			else if (Turn == Players.Player2)
+			{
+				int? searchCurrentInput = Player2Inputs.FirstOrDefault(i => currentInput == currentInput);
+
+				return searchCurrentInput == null ? false : true;
+			}
+			return false;
+			 */
+			return Player1Inputs.Contains(currentInput) || Player2Inputs.Contains(currentInput);
+
+		}
+
+		public void SwitchPlayers()
+		{
+			if (Turn == Players.Player1)
+			{
+				Turn = Players.Player2;
+			}
+			else
+			{
+				Turn = Players.Player1;
+			}
+		}
+
+		public (bool IsWin, Players? WinningPlayer) CheckForWin()
+		{
+			Players? winningPlayer = null;
+
+			if (Turn == Players.Player1)
+			{
+				foreach (var wcombination in winningCombinations)
+				{
+					if (wcombination.All(p => Player1Inputs.Contains(p)))
+					{			
+						winningPlayer = Players.Player1;
+						return (true, winningPlayer);
+					}
+					else
+					{
+						return (false, null);
+					}
+				}
+			}
+
+			else if (Turn == Players.Player2)
+			{
+
+				foreach (var wcombination in winningCombinations)
+				{
+					if (wcombination.All(p => Player2Inputs.Contains(p)))
+					{
+						winningPlayer = Players.Player2;
+						return (true, winningPlayer);
+					}
+					else
+					{
+						return (false, null);
+					}
+				}
+			}
+			return (true, null);
+
+		}
+
+		public void Restart()
+		{
+			Player1Inputs.Clear();
+			Player2Inputs.Clear();
+			Turn = Players.Player1;
+		}
+	}
+}
